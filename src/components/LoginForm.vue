ed888b0b382bcacde0cd9517fb8de910bda314d0
yoga <abiyoga1335@gmail.com>
2020-04-19 12:20:09 +0700

    initial LoginForm testing

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <input type="text" v-model="name" />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('formSubmitted', { name: this.name })
+    }
+  }
+}
+</script>
